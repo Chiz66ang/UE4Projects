b5975e8e7a123e06dbec3fcbfe7ad24893534487
Chiz66ang <redhat20142014@gmail.com>
2020-01-12 17:43:00 +0900

    ShootProject

---
+++ ShootProject/Source/ShootProject/ShootProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "ShootProject.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShootProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AShootProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AShootProjectGameMode();
+};
+
+
+
